7d525f884313411ea8dc91cc7f3162cd8c48c64e
Aonrafay <aonrafay@gmail.com>
2025-07-08 21:21:32 +0500

    Add files via upload

---
+++ basicsofPython.py
@@ -0,0 +1,23 @@
+A,B=13,23
+c=A/B
+D=A//B
+print("The value of c is:")
+print(c)
+print("The value of D is:")
+print(D)
+print("The type of c is:")
+print(type(c))
+print("The type of D is:")
+print(type(D))
+C,D=-5,2
+print("The value of C%D is:", C%D)
+print("The value of D%C is:", D%C)
+
+#Take input from user
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+salary=input("Enter your salary: ")
+print("Your name is:", name)
+print("Your age is:", age)
+print("Your salary is:", salary)
+
