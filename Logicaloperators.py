98ab2aed0b90216cb3dee0fdab9b8e98e5af4b6c
Aonrafay <aonrafay@gmail.com>
2025-07-10 01:05:29 +0500

    Add files via upload

---
+++ Logicaloperators.py
@@ -0,0 +1,5 @@
+print(not True)  # Output: False
+print(not False)  # Output: True
+print(True and True)  # Output: True
+print(True and False)  # Output: False
+print(False and True)  # Output: False
