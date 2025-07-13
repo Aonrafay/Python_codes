2e103c95713d97c7531c3ddaca88f4136e5bc8cd
Aonrafay <aonrafay@gmail.com>
2025-07-13 22:26:21 +0500

    Add files via upload

---
+++ stringcatenation.py
@@ -0,0 +1,8 @@
+str="This is the python testimonail"
+print(str)
+
+str1="Good work by Muhammad Aon Rafay "
+str2=" and his team"
+print(str1 + str2)  # Output: Good work by Muhammad Aon Rafay and his team
+print(str1 * 2)  # Output: Good work by Muhammad Aon Rafay Good work by Muhammad Aon Rafay
+print(str1 + " " + str2)  # Output: Good work by Muhammad Aon Rafay and his team
