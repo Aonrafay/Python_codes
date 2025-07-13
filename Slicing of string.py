07ef72a1a7eb0d05a8911cda1baf74c4d610019e
Aonrafay <aonrafay@gmail.com>
2025-07-13 22:26:37 +0500

    Add files via upload

---
+++ Slicing of string.py
@@ -0,0 +1,16 @@
+str="This is the python testimonail"
+print(str)
+# Slicing the string
+print(str[0:4])  # Output: This
+print(str[5:7])  # Output: is
+print(str[8:11])  # Output: the
+print(str[12:19])  # Output: python
+print(str[20:29])  # Output: testimonail
+print(str[30:])  # Output:  # Empty string
+print(str[0:])  # Output: This is the python testimonail
+print(str[-1])  # Output: l
+print(str[-2])  # Output: i
+print(str[-3])  # Output: a
+print(str[-4])  # Output: n
+print(str[-5])  # Output: o
+print(str[-12])  # Output: space
