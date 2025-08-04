b4b880abf09f8603b91a48045c4730cc0074a004
Aonrafay <aonrafay@gmail.com>
2025-08-04 23:10:48 +0500

    Add files via upload

---
+++ modifying a srting.py
@@ -0,0 +1,5 @@
+str="Hello World"
+print(str.upper())  # Convert to uppercase
+print(str.lower())  # Convert to lowercase
+print(str.replace("World", "Python"))  # Replace substring
+print(str.split())  # Split the string into a list
