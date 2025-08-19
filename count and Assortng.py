8dd04fa99a44fe829e671dfd83b47fa3ae0d54a8
Aonrafay <aonrafay@gmail.com>
2025-08-19 12:59:16 +0500

    Add files via upload

---
+++ count and Assortng.py
@@ -0,0 +1,19 @@
+list=(2,3,3,2,4,1,4,5,3,4,2,1,3,4,5,2,3,4,5)
+#for counting the number of times an item appears in a list
+count_of_2 = list.count(2)
+print("Count of 2:", count_of_2)  # Output: Count of
+
+count_3=list.count(3)
+print("Count ofthe 3:",count_3)
+
+count_4=list.count(4)
+print("Count of 4:", count_4)  # Output: Count of 4: 5
+
+print(type(list))  
+
+# #for assorting the list
+# list.sort()  # Sort the list in ascending order
+# print("Sorted list:", list) 
+# list.reverse()  # Reverse the list
+# print("Reversed list:", list)  # Output: Reversed list: 
+# list.sort(descending=True)  # Sort the list in descending order
