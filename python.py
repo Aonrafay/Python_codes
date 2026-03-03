9b058bf0acf814bc88ec7fff2affae063bc5eac4
Aonrafay <185882734+Aonrafay@users.noreply.github.com>
2026-03-03 23:30:55 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,32 @@
+print("Hello World")
+print("Muhammad Aon Rafay")
+print("I am learning Python programming.")
+print("Python is fun!")
+print("Let's code more in Python.")
+print("Goodbye!")
+print("See you later!")
+print("Have a great day!")
+print("Keep practicing!")
+print("Stay curious!")
+print("Embrace challenges!")
+print("Never stop learning!")
+print("Believe in yourself!")
+
+print (3 + 4)
+
+print (10 - 2)
+
+print (5 * 6)
+
+x=8+2+5+7
+print (x)
+y=x-3
+print (y)
+z=y*2
+print (z)
+
+print('''
+This is a multi-line string.
+It can span multiple lines.
+Python makes it easy!
+      ''')
